a0f707cefeedd7b700fe2202c7985c20b4b9c20c
Nebojša Vuga <vuga.sv53.2020@uns.ac.rs>
2023-11-29 11:44:45 +0100

    Updated docker info

--- Web Of Things/Vezbe/vezbe 6/InfluxDB/server.py
+++ Web Of Things/Vezbe/vezbe 6/InfluxDB/server.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 
 
 # InfluxDB Configuration
-token = "8Ok9ToG_j8Vh6vt_wavJZFy_pRVzFYGs4hSvdvjcBjTa5Ju847ZeFOVKEy5F2VPKXRS7U552ek8PszafQ4xG9Q=="
+token = "HyQv26GM6CCyEU34h0ydxSlecOaGTC1pvDcBOwL5WmNSsZDFQ28AKPfSo8kt-Pbimbvyhy_ali7UJN-1tIyoBg=="
 org = "FTN"
 url = "http://localhost:8086"
 bucket = "example_db"
